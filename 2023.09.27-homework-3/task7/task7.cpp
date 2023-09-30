2b2dc79b74a633cab004c22f970d09ab07ce5326
st117161 <st117161@student.spbu.ru>
2023-09-30 22:01:27 +0300

    lesson 3 code upload

--- 2023.09.27-homework-3/task7/task7.cpp
+++ 2023.09.27-homework-3/task7/task7.cpp
@@ -2,17 +2,5 @@
 
 int main()
 {
-    long int n = 0;
-    long int k = 0;
-    long int i = 1;
-    std::cin >> n;
-
-    while(i < n)
-    {
-        i = i * 2;
-        k++;
-    }
-    
-    std::cout << k << std::endl;
     return EXIT_SUCCESS;
 }
